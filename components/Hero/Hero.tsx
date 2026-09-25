ab8109dbd6b58dbc2b10d2c2be04033ad45abb84
unknown <barretodairatamara@gmail.com>
2026-09-25 19:46:13 -0300

    Mejora responsive

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ import heroImage from "@/assets/LOGOS/CONGRESO ETS/Logo_Cong_ETS_blanco.svg";
 const Hero = () => {
     return (
         <section className="bg-azul-oscuro">
-        <div className="flex min-h-[520px] flex-col items-center justify-center text-white px-6 py-10">
+            <div className="flex min-h-[520px] flex-col items-center justify-center text-white px-6 py-10">
 
                 {/* Nombre del Congreso */}
                 <h4 className="text-lg font-medium uppercase tracking-[0.2em] text-white/80 md:text-xl">
@@ -20,7 +20,21 @@ const Hero = () => {
                 />
 
                 {/* Fecha y ubicación */}
-                <HeroAddress text="6 de noviembre · Auditorio Polo Saavedra 5085 · Buenos Aires" />
+                <HeroAddress
+                    text={
+                        <>
+                            <span className="block md:inline">
+                                6 de noviembre
+                            </span>
+
+                            <span className="hidden md:inline"> · </span>
+
+                            <span className="block md:inline">
+                                Universidad de la Ciudad de Buenos Aires · Tte. Gral. Juan Domingo Perón 802
+                            </span>
+                        </>
+                    }
+                />
 
                 {/* Botón de inscripción */}
                 <HeroButton text="Inscribirse" />
